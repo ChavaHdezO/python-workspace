da247d628176ded2701fa47cb4c1c586fb0cf4cd
chava <chava.490@gmail.com>
2023-05-31 21:45:30 -0600

    Creating dictionaries

---
+++ tuples_dictionaries_sets/creating_dictionaries.py
@@ -0,0 +1,67 @@
+"""
+locations = {
+    "headquarters": "New York",
+    "flagship": "Paris"
+}
+
+print(locations)
+"""
+"""
+car_data = {
+    "brand" : "Cadillac",
+    "year" : 1960,
+    "restoration" : ["1990", "2018"],
+    "rented" : False
+}
+
+print(car_data)
+"""
+"""
+contents = {
+    "ch. 1" : "A long expected party",
+    "ch. 2" : "The shadow of the past",
+    "ch. 3" : "Three is company"
+}
+"""
+"""
+user = {
+    "name" : "Gilles",
+    "surname" : "Deleuze"
+}
+
+print(user)
+"""
+"""
+participants = {
+    "Meg" : True,
+    "Kim" : False,
+    "Luis" : True,
+    "Luis M." : False
+}
+
+print(participants)
+"""
+"""
+members_count = {
+    ("Sai", "Chloe", "Yumi") : 3
+}
+
+print(members_count)
+"""
+"""
+winner_scores = {
+    "first" : ("Ted", 50),
+    "second" : ("Jess", 47)
+}
+
+print(winner_scores)
+"""
+air_composition = {
+    "nitrogen" : "78%",
+    "oxygen" : "21%",
+    "argon" : "0.93%",
+    "carbon dioxide" : "0.04%",
+    "other" : "0.03%"
+}
+
+print(air_composition)
