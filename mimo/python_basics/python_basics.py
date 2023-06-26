b17542ac49474a906069e4fb799c27020d281dee
chava <chava.490@gmail.com>
2023-06-26 15:05:30 -0600

    Mimo - Python - Python Basics

---
+++ mimo/python_basics/python_basics.py
@@ -0,0 +1,32 @@
+"""
+
+msg = "Hello World"
+print(msg)
+
+print("Hello!")
+
+mostViewed = "7 Amazing Mugs I CANNOT live without"
+leastViewed = "The Best Mug to Take Scuba Diving"
+
+print(mostViewed)
+print(leastViewed)
+
+views = 1145
+comments = 200
+
+print(views)
+print(comments)
+"""
+menu_item = "pizza"
+print(menu_item)
+menu_item = "biryani"
+guests = 10
+print(guests)
+print("Updated menu item is: " + menu_item)
+biryani_per_person = 1
+biryani_needed = biryani_per_person * guests
+biryani_prepared = 10
+enough_biryani = biryani_prepared == biryani_needed
+guests += 1
+biryani_per_guest = biryani_prepared / guests
+print(biryani_per_guest)
